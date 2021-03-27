a8a4f1b078840d36f860e7e3dff1d8e32ef3daa1
Mike Wood <subset.of.insanity@gmail.com>
2021-03-27 21:32:27 +0000

    Add layer for Rail stops

--- import_pubs.py
+++ import_pubs.py
@@ -1,10 +1,10 @@
 import geopandas as gp
 
-gdf = gp.read_file("pubs.geojson")
+gdf = gp.read_file("rail.geojson")
 #gdf.set_crs('epsg:4326')
 gdf['centroid'] = gdf.centroid
 gdf = gdf.set_geometry('centroid', drop=True)
-with open('centroids.geojson', 'w') as centroid_file:
+with open('railCentroids.geojson', 'w') as centroid_file:
     centroid_file.write(gdf.to_json(na='drop'))
 #gdf.to_file("centroids.geojson", driver="GeoJSON")
 
